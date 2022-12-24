530743057e81afee9c8d8753aecd8be0f42034b6
phuong090802 <93528419+phuong090802@users.noreply.github.com>
2022-12-24 21:44:19 +0700

    Final Project CP

--- Final/Build-NodeJS-with-Mysql/app/config/db.config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv/config');
-
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root123",
-    DB: "cloud_computing"
-}
--- Final/Build-NodeJS-with-Mysql/app/routes/route.js
+++
@@ -1,5 +0,0 @@
-module.exports = app => {
-    require('./auth.route')(app);
-    require('./todo.route')(app);
-    require('./web.route')(app);
-}
